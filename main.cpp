17938503e1c1e70419314106bac1a78a8b481cf4
john <polpokpol2@gmail.com>
2024-12-22 17:58:02 +0800

    further cleanup part 1

--- main.cpp
+++ main.cpp
@@ -83,19 +83,25 @@ string left_and_right_encoding(u8 champiArray[], u8 counter[])
                 } break;
                 // ------------------
                 case 0b01: {
-                    counter[1] = 3; // this is for debug only cause i did not configure vscode debug yet
+                    // counter[1] = 3; // this is for debug only cause i did not configure vscode debug yet
                     if(wide){
                         s_reg += nmemonics::umap11_wide.at(input_reg); 
-                        // counter[0] +=1;
+                        // counter[1] = 122;
                     }else{
                         s_reg += nmemonics::umap11_not_wide.at(input_reg);
+                        counter[1] = 122;
                     }
                     counter[0] +=1;
-                    string stringified_integer_data = _transform_int_to_string(champiArray[index + 3]);
+                    string stringified_integer_data = "";
+                    if( champiArray[index + 2] != 0){
+                        stringified_integer_data = " + " +  _transform_int_to_string(champiArray[index + 2]);
+                    }
+
                     s_rm += nmemonics::umap_rm_wide.at(input_rm);
                     if (destination) {
                         // nmemonics::umap.at(input) + " " + s_reg + ", " + s_rm + " + " + stringified_integer_data
-                        return nmemonics::umap.at(input) + " " + s_reg + ", " + _bracketer((s_rm + " + " + stringified_integer_data));
+                        // counter[1] = 122;
+                        return nmemonics::umap.at(input) + " " + s_reg + ", " + _bracketer((s_rm +  stringified_integer_data));
                     }
                     return nmemonics::umap.at(input) + " " + s_rm + ", " + s_reg;
                 } break;
@@ -109,7 +115,11 @@ string left_and_right_encoding(u8 champiArray[], u8 counter[])
                     }
                     s_rm += nmemonics::umap_rm_wide.at(input_rm);
                     if (destination) {
-                        return nmemonics::umap.at(input) + " " + s_reg + ", " + s_rm;
+                        string nmemonics_result = nmemonics::umap.at(input) + " ";
+                        string s_reg_result = s_reg + ", ";
+                        string s_rm_result = s_rm;// we may add if needed later
+                        string result_0b00 = nmemonics_result + s_reg_result + _bracketer( s_rm_result);
+                        return result_0b00;
                     }
                     return nmemonics::umap.at(input) + " " + s_rm + ", " + s_reg;
                 } break;
@@ -149,10 +159,11 @@ int main(int argc, char **argv)
     //     0b10011, 0b10001001, 0b1001, 0b10001000, 0b1010, 0b10001000, 0b1101110, 0b0
     // }; // temporary2, we will use external file later
 
-    // u8 champiHex[] = {0b10001001, 0b11011110, 0b10001000, 0b11000110, 0b10110001, 0b1100, 0xb5, 0xf4, 0xb9, 0x0c, 0x0,
-    // 0b10111001, 0b11110100, 0b11111111, 0b10111010, 0b01101100, 0b1111, 0xba, 0x94, 0xf0, 0x8a, 0b0, 0x8b, 0x1b,
-    // 0x8b, 0x56, 0x00};
-    u8 champiHex[] = {0b10001010, 0b01100000, 0b00000100};
+    u8 champiHex[] = {0b10001001, 0b11011110, 0b10001000, 0b11000110, 0b10110001, 0b1100, 0xb5, 0xf4, 0xb9, 0x0c, 0x0,
+    0b10111001, 0b11110100, 0b11111111, 0b10111010, 0b01101100, 0b1111, 0xba, 0x94, 0xf0, 0x8a, 0b0, 0x8b, 0x1b,
+    0x8b, 0x56, 0x00};
+    // u8 champiHex[] = {0xba, 0x94, 0xf0};
+    // u8 champiHex[] = {0b10001011, 0b01010110, 0b0};
 
     string result = "";
     u8 kopal[] = {0, 0}; // it is an array as we will store many more later here
@@ -162,8 +173,10 @@ int main(int argc, char **argv)
         kopal[0]+=2;
         std::cout << result << std::endl;
     }
-    printf("lalala: %d\n", kopal[0]);
-    cout << _transform_int_to_string(20) << "  asdasda" << endl;
+    printf("lalala counter0: %d\n", kopal[0]);
+    printf("lalala counter1: %d\n", kopal[1]);
+    printf("lalala ArrayCountChampi: %d\n", ArrayCount(champiHex));
+    // cout << _transform_int_to_string(20) << "  asdasda" << endl;
 
     // string toli = "lala ";
     // toli = toli + std::to_string(10);
